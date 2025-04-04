d623038747ceea10ae9b5cc584e6c0bb9ae98135
GamalTolan <gamaltolan4@gmail.com>
2025-04-04 13:34:56 +0200

    Assignment05MVC

---
+++ AssignmentMVC04/Models/ResetPassWordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssignmentMVC04.Models
+{
+    public class ResetPassWordViewModel
+    {
+        [Required(ErrorMessage = "Password Name is Required")]
+
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm Password Name is Required")]
+        [Compare(nameof(Password), ErrorMessage = "Password Dose not Match Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        public string Token { get; set; }
+
+        public string Email { get; set; }
+    }
+}
